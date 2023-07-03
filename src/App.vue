ad1456b8f886d7c3c134ae862cf3152669aae026
hy918 <2385499673@qq.com>
2023-07-03 22:17:51 +0800

    生成eslint文件，但未配置

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
-    <div>
-        <h1>我是标题</h1>
-    </div>
-    
+  <div>
+    <h1>我是标题</h1>
+  </div>
 </template>
-<script setup lang="ts">
-
-</script>
-<style lang="">
-    
-</style>
+<script setup lang="ts"></script>
+<style lang=""></style>
